93f8a498911f1c39c1a522087fb3584abac557b4
okarp <ky6034@kolumbus.fi>
2021-02-26 14:24:25 +0200

    feat: refactor API call into Quiz component

--- src/components/Questions.vue
+++ src/components/Questions.vue
@@ -1,45 +1,38 @@
 <template> 
-    <div>
-
-      <h1 v-html="loading ? 'Loading questions from the api...' : currentQuestion.question"></h1>
-
+    <div>      
+      <h1>{{currentQuestion.question}}</h1>
       <div v-if="currentQuestion">
         <button type="button" v-for="answer in currentQuestion.answers"
         :key="answer" @click="handleClick(answer)">{{answer}}</button>                    
-      </div>  
-
+      </div> 
       </div>   
 </template>
 
 <script>
-import {getQuestions} from '../utils/questions.js'
-
 export default {
+  props:{
+    questions: Array
+  },
   data() {
     return {
       index: 0,
       points: 0,
-      loading: true,
-      questions: [],
+      loading: true,      
       userAnswers: [],      
     }
   },  
   //returns a single question from the list of questions
   computed: {
     currentQuestion() {
+      if (this.questions != null){
       console.log(this.questions)      
       return this.questions[this.index];
     }
+    return "";
+    }
   },
-  methods: {
-    //Knuth shuffle for answer array
-    shuffleArray(array) {        
-        for (let i = array.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [array[i], array[j]] = [array[j], array[i]];
-        }       
-        return array;        
-    },
+
+    methods: {
     //Handles the click of answer buttons. Gets the users answer as a param.
     handleClick(answer) {
     //if answer was correct increment points by 10
@@ -56,23 +49,8 @@ export default {
       if (this.index == this.questions.length){        
         this.$parent.updateResults(this.userAnswers, this.points)            
       }
-    },
-    async loadQuestions() {
-      this.questions = await getQuestions();
-      //Map incorrect answers and the correct answer into shuffled answers array
-      this.questions.map((question) => {
-        question.answers = this.shuffleArray([
-          question.correct_answer,
-          ...question.incorrect_answers]);
-      });
-      //when data has been loaded set loading as false
-      this.loading = false;
-    },
-  },
-  //load questions when component gets created
-  mounted() {
-    this.loadQuestions();
-  },
+    }
+  }     
 }
 </script>
 <style>
--- src/components/Quiz.vue
+++ src/components/Quiz.vue
@@ -1,20 +1,25 @@
 <template> 
-    <div>       
-      <Questions v-if="!quizEnded" />          
-      <Results :userAnswers="userAnswers" :points="points" v-if="quizEnded"/>
+    <div> 
+      <div v-if="!loading">      
+      <Questions :questions = "questions" v-if="!quizEnded" />   
+      </div>       
+      <Results :userAnswers = "userAnswers" :points="points" v-if="quizEnded"/>
     </div> 
 </template>
 
 <script>
 import Results from './Results'
 import Questions from './Questions'
+import {getQuestions} from '../utils/questions.js'
 
 export default {
   data() {
     return {
       points: 0,
       quizEnded: false,
+      questions: [],
       userAnswers: [],
+      loading: true,
     }
   },
   components: {
@@ -22,13 +27,36 @@ export default {
     Questions
   },
   methods: {
-    updateResults(userAnswers, points){
+    updateResults(userAnswers, points) {
       this.points = points;
-      this.userAnswers = userAnswers;   
-      this.quizEnded = true;         
-    }
+      this.userAnswers = userAnswers;
+      this.quizEnded = true;
+    },
+    async loadQuestions() {
+      this.questions = await getQuestions();
+      //Map incorrect answers and the correct answer into shuffled answers array
+      this.questions.map((question) => {
+        question.answers = this.shuffleArray([
+          question.correct_answer,
+          ...question.incorrect_answers
+        ]);
+      });
+      //when data has been loaded set loading as false
+      this.loading = false;
+    },
+
+    //Knuth shuffle for answer array
+    shuffleArray(array) {
+      for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+      }
+      return array;
+    },
   },
-  
+  created() {
+    this.loadQuestions();
+  },
 }
 </script>
 
