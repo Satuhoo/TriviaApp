dc77ee1a72647d4baed762e87526121b1d7dab1b
satu.heikkonen <satu.heikkonen@5CG05206R0.nordic.local>
2021-02-26 15:21:28 +0200

    fix: start game button is shown after quit game

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <Header />
-    <button @click="openGame" v-show="showButton">Start game</button>
+    <button @click="openGame" v-if="$route.path === '/'">Start game</button>
     <router-view>
       <Quiz/>
     </router-view>
@@ -12,22 +12,15 @@
 <script>
 import Header from "./components/Header";
 import Quiz from "./components/Quiz";
-
 export default {
   name: "App",
   components: {
     Header,
     Quiz,
   },
-  data() {
-    return {
-      showButton: true,
-    };
-  },
   methods: {
     openGame() {
       this.$router.push('/quiz');
-      this.showButton = false;
     },
   },
 };
